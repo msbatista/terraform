fcf318bd7fdb208e61281885a4567ab71ba8efde
msbatista <silvabatistamarcelo@gmail.com>
2021-02-20 14:59:33 -0300

    Create Virtual Machine azure

--- learn-terraform-azure/main.tf
+++ learn-terraform-azure/main.tf
@@ -18,6 +18,111 @@ provider "azurerm" {
 }
 
 resource "azurerm_resource_group" "rg" {
-  name     = "rg-msb-uat"
+  name     = "rg-msb-org"
   location = "eastus2"
+
+  tags = {
+    Environment = "Terraform Getting Started"
+    Teams       = "DevOps"
+  }
+}
+
+resource "azurerm_virtual_network" "vnet" {
+  name                = "vnetmsborg"
+  address_space       = ["10.0.0.0/16"]
+  location            = "eastus2"
+  resource_group_name = azurerm_resource_group.rg.name
+}
+
+variable "admin_username" {
+  type        = string
+  description = "Admin user name for virtual machine"
+}
+
+variable "admin_password" {
+  type        = string
+  description = "Password must meet Azure complexity requirements"
+}
+
+resource "azurerm_subnet" "subnet" {
+  name                 = "subnetmsborg"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
+
+resource "azurerm_public_ip" "publicip" {
+  name                = "msbpubip"
+  location            = "eastus2"
+  resource_group_name = azurerm_resource_group.rg.name
+  allocation_method   = "Static"
+}
+
+resource "azurerm_network_security_group" "nsg" {
+  name                = "nsgmsb"
+  location            = "eastus2"
+  resource_group_name = azurerm_resource_group.rg.name
+
+  security_rule {
+    name                       = "SSH"
+    priority                   = 1001
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+}
+
+resource "azurerm_network_interface" "nic" {
+  name                = "nicmsb"
+  location            = "eastus2"
+  resource_group_name = azurerm_resource_group.rg.name
+
+  ip_configuration {
+    name                          = "niconfigmsb"
+    subnet_id                     = azurerm_subnet.subnet.id
+    private_ip_address_allocation = "dynamic"
+    public_ip_address_id          = azurerm_public_ip.publicip.id
+  }
+}
+
+resource "azurerm_virtual_machine" "vm" {
+  name                  = "vmmsb"
+  location              = "eastus2"
+  resource_group_name   = azurerm_resource_group.rg.name
+  network_interface_ids = [azurerm_network_interface.nic.id]
+  vm_size               = "Standard_DS1_v2"
+
+  storage_os_disk {
+    name              = "vmmsbosdisk"
+    caching           = "ReadWrite"
+    create_option     = "FromImage"
+    managed_disk_type = "Premium_LRS"
+  }
+
+  storage_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "16.04.0-LTS"
+    version   = "latest"
+  }
+
+  os_profile {
+    computer_name  = "vmmsb0221"
+    admin_username = var.admin_username
+    admin_password = var.admin_password
+  }
+
+  os_profile_linux_config {
+    disable_password_authentication = false
+  }
+}
+
+data "azurerm_public_ip" "ip" {
+  name                = azurerm_public_ip.publicip.name
+  resource_group_name = azurerm_virtual_machine.vm.resource_group_name
+  depends_on          = [azurerm_virtual_machine.vm]
 }
